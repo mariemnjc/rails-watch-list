b2ff372a049ae272e65015f5e93775167a8ae7eb
Marie Menjucq <mariemnjc@gmail.com>
2025-03-07 09:33:31 +0100

    Bookmark done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
+require "json"
+
+url = "https://tmdb.lewagon.com/movie/top_rated";
+
+
+10.times do
+  movies = JSON.parse(URI.open(url).read)["results"]
+
+  movies.each do |movie|
+     Movie.create(
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+      rating: movie["vote_average"]
+    )
+  end
+end
